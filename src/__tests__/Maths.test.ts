9e6901ca06462df00391159e882cae0bb30e16a2
rohan099 <rohansharma@betalectic.com>
2022-04-21 18:30:11 +0530

    Setup Package

---
+++ src/__tests__/Maths.test.ts
@@ -0,0 +1,4 @@
+let Maths = require('../index')
+test('My SUM', () => {
+  expect(Maths.Sum(1,2)).toBe(3);
+});
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+const Sum = (...numbers: number[]) => {
+  let aggregateNumber = 0;
+  for (const digit of numbers) aggregateNumber += digit;
+  return aggregateNumber;
+};
+
+const Multiply = (...numbers: number[]) => {
+  let aggregateNumber = 1;
+  for (const digit of numbers) aggregateNumber *= digit;
+  return aggregateNumber;
+};
+
+module.exports = {
+  Sum,
+  Multiply,
+};
